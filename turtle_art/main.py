4ed5edef799a30a6d2341a85773c11d3cf744c41
gitmott <135948764+gitmott@users.noreply.github.com>
2023-09-28 21:27:27 +1000

    start turtle

---
+++ turtle_art/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_turtle = Turtle()
+timmy_turtle.shape("turtle")
+timmy_turtle.color("DarkViolet")
+timmy_turtle.pen(fillcolor="DarkViolet", pencolor="black")
+timmy_turtle.pendown()
+# timmy_turtle.pensize(3)
+
+timmy_turtle.fd(10)
+timmy_turtle.left(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

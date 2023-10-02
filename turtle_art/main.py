8924babfc8ab0d2d2ed1c3d1f2d75efb55e3c7f1
gitmott <135948764+gitmott@users.noreply.github.com>
2023-10-03 08:00:50 +1100

    drawing shapes

--- turtle_art/main.py
+++ turtle_art/main.py
@@ -1,14 +1,43 @@
 from turtle import Turtle, Screen
 
-timmy_turtle = Turtle()
-timmy_turtle.shape("turtle")
-timmy_turtle.color("DarkViolet")
-timmy_turtle.pen(fillcolor="DarkViolet", pencolor="black")
-timmy_turtle.pendown()
-# timmy_turtle.pensize(3)
+tt = Turtle()
+tt.shape("turtle")
+tt.color("DarkViolet")
+tt.pen(fillcolor="DarkViolet", pencolor="black")
+tt.pensize(5)
 
-timmy_turtle.fd(10)
-timmy_turtle.left(10)
+
+# triangle
+for side in range(3):
+    tt.pd()
+    tt.fd(100)
+    tt.right(120)
+
+# square
+for side in range(4):
+    tt.pd()
+    tt.fd(100)
+    tt.right(90)    
+# pent
+for side in range(5):
+    tt.pd()
+    tt.fd(100)
+    tt.right(360/5)    
+# hex
+for side in range(6):
+    tt.pd()
+    tt.fd(100)
+    tt.right(360/6)    
+# sev
+for side in range(7):
+    tt.pd()
+    tt.fd(100)
+    tt.right(360/7)    
+# oct
+for side in range(8):
+    tt.pd()
+    tt.fd(100)
+    tt.right(360/8)    
 
 
 

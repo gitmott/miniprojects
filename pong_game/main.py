348cf551d3d68ce9817b49157e2cf7b7e053c9e8
gitmott <135948764+gitmott@users.noreply.github.com>
2023-10-25 19:06:14 +1100

    Add paddle movement

--- pong_game/main.py
+++ pong_game/main.py
@@ -4,10 +4,22 @@ from paddles import Paddle
 screen = Screen()
 paddle = Paddle()
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
 
 screen.setup(width=800, height=600)
 screen.bgcolor("Black")
 screen.title("Pong")
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 paddle.create_paddles_right((350, 0))
 # paddle.create_paddles_left((-350, 0))

f0d564f37fb33fae03f7f597182494930099bf86
gitmott <135948764+gitmott@users.noreply.github.com>
2023-10-27 19:21:55 +1100

    Add scoring mechanic

--- pong_game/scoreboard.py
+++ pong_game/scoreboard.py
@@ -9,14 +9,20 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 60, "normal"))
         self.goto(100, 200)
-        self.write(self.l_score, align="center", font=("Courier", 60, "normal"))
+        self.write(self.r_score, align="center", font=("Courier", 60, "normal"))
 
     def add_score_right(self):
         self.r_score += 1
+        self.update_scoreboard()
 
     def add_score_left(self):
         self.l_score += 1
+        self.update_scoreboard()
 

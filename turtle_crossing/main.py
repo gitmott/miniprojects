57219c1501d942dfb6fc0eaa775e6555497519f8
gitmott <135948764+gitmott@users.noreply.github.com>
2023-10-28 17:09:48 +1100

    Create scoring system

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -16,12 +16,25 @@ screen.onkey(player.move, "Up")
 player.create_turtle()
 player.refresh_turtle()
 
-scoreboard.create_scoreboard()
+scoreboard.update_scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if player.ycor() > -120:
+        player.refresh_turtle()
+        scoreboard.update_scoreboard()
+
 
 
 screen.exitonclick()
+
+
+# TODO Fix scoeboard
+# TODO create scoring when turtle gets to end (y=200??)
+# TODO refresh when turtle gets to end
+# TODO add score when turtle gets to end
+# TODO create cars
+# TODO increase car speed when turtle reaches end
+# TODO create car collision, gameover condition
--- turtle_crossing/scoreboard.py
+++ turtle_crossing/scoreboard.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
 
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
     
     def __init__(self):
         super().__init__()
+        self.level = 0
+        self.hideturtle()
+        self.penup()
+        self.update_scoreboard()
 
-    def create_scoreboard(self):
-        self.write( FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-200, 250)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+        self.level += 1
+
+

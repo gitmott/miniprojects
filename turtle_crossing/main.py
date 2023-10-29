738af6594bb2c0e660dd427f98cd3acb5fd7c815
gitmott <135948764+gitmott@users.noreply.github.com>
2023-10-29 16:41:16 +1100

    Create cars, collisions, level up speed increase

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -4,8 +4,10 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 player = Player()
 scoreboard = Scoreboard()
+car_manager = CarManager()
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -22,10 +24,26 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_car()
+
+
+
     if player.ycor() > -120:
         # TODO increase car speed
         player.refresh_turtle()
         scoreboard.update_scoreboard()
+        car_manager.move_car()
+        car_manager.increase_car_speed()
+
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over_message()
+    
+
 
 
 screen.exitonclick()

e2958d0193fe697e9a67461150801e09193b3300
gitmott <135948764+gitmott@users.noreply.github.com>
2023-11-01 21:56:09 +1100

    Start creating answer mapping

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -12,17 +12,26 @@ turtle.shape(image)
 # Setup states list
 data = pandas.read_csv(r"C:\Users\Matt\repos\miniprojects\us_states_game\50_states.csv")
 state_list = []
-correctly_guessed_states = []
 state_column = data.state
 for state in state_column:
     state_list.append(state)
 
+correctly_guessed_states = []
 score = 0
 
 game_on = True
 while game_on:
     answer_state = screen.textinput(title="Guess the State", prompt="Guess a state")
-    if answer_state.title() in state_list:
+    answer_state = answer_state.title()
+    if answer_state in state_list:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        row_data = data[data.state == answer_state]
+        t.goto(int(row_data.), int(row_data.y))
+
+
+
         score += 1
         correctly_guessed_states.append(answer_state)
         # state_list.remove(answer_state)

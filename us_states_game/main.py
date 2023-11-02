43cee6d9fdd2cf0932182ae3396db962f1943c2c
gitmott <135948764+gitmott@users.noreply.github.com>
2023-11-02 22:22:49 +1100

    Add exit function. Create csv with missed states

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -22,29 +22,28 @@ score = 0
 
 while len(correctly_guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(correctly_guessed_states)}/50 States guessed", prompt="Guess a state").title()
+    
+    if answer_state.title() == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in correctly_guessed_states:
+                missing_states.append(state)
+        missed_states_data = pandas.DataFrame(missing_states)
+        missed_states_data.to_csv("states_to_practise.csv")
+        break
+        
     if answer_state in state_list:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         row_data = data[data.state == answer_state]
         t.goto(int(row_data.x.iloc[0]),int(row_data.y.iloc[0]))
-        print("Working")
         t.write(row_data.state.item())
-
-
-
         score += 1
         correctly_guessed_states.append(answer_state)
         # state_list.remove(answer_state)
-    else:
-        game_on = False
 
 print(len(state_list))
 print(len(correctly_guessed_states))
-
-
-
-
-
 
 screen.exitonclick()

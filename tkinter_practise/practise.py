fe8688a8a4c62bcc03793acab9fa3b9deb342f64
gitmott <135948764+gitmott@users.noreply.github.com>
2023-11-05 14:44:29 +1100

    Rename main to pracitse

--- tkinter_practise/practise.py
+++ tkinter_practise/practise.py
@@ -1,29 +1,36 @@
 import tkinter
 
+def button_click():
+    user_input = input.get()
+    # my_label.config(text= f"{user_input}")
+
+
 window = tkinter.Tk()
 window.title("GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
-
+# Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "underline"))
-my_label.pack()
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
 
 my_label["text"] = "New Text"
-# my_label.config(text="New Text")
 
 
 
-def button_click():
-    user_input = input.get()
-    my_label.config(text= f"{user_input}")
 
-
+# Button
 button = tkinter.Button(text="egg", command=button_click)
-button.pack()
+button.grid(column=1, row=1)
+
+# New button
+new_button = tkinter.Button(text="new_egg", command=button_click)
+new_button.grid(column=2, row=0)
 
 # entry 
 input = tkinter.Entry(width= 10)
-input.pack()
+input.grid(column=3, row=2)
 # user_input = input.get()
 
 

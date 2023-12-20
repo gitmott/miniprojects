9fea3b0577aafcae90bc190b13f60a130ac07ac0
gitmott <135948764+gitmott@users.noreply.github.com>
2023-12-20 20:56:35 +1100

    Add exception handling

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,8 +1,19 @@
 import pandas
 
-data = pandas.read_csv(r"C:\Users\Matt\repos\miniprojects\NATO-alphabet-start\nato_phonetic_alphabet.csv")
+data = pandas.read_csv(r"C:\Users\Matt\repos\miniprojects\password_manager\nato_phonetic_alphabet.csv")
 raw_df = pandas.DataFrame(data)
+
 data_dict = {row.letter:row.code for (index, row) in raw_df.iterrows()}
-word = input("Enter a word: ").upper()
-nato_dict = [data_dict[letter] for letter in word]
-print(nato_dict)
+
+
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_dict = [data_dict[letter] for letter in word]
+    except KeyError:
+        print("Wrong data type. Try again.")
+        generate()
+    else:
+        print(nato_dict)
+
+generate()
